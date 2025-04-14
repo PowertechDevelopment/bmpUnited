1072eafa9bb190682eefdce2f986b24d3f68127e
Elisha Delight <powertech.development1@gmail.com>
2025-04-14 12:35:22 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          bangers: ['Bangers', 'cursive'],
+          carter: ['Carter One', 'cursive'],
+          ovo: ['Ovo', 'serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
